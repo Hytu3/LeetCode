bc09e37df6b135067d1d91c5ecd0dc5672fd4ec4
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-02-13 11:19:26 -0700

    Time: 6 ms (12.72%) | Memory: 45.5 MB (42.17%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,41 +1,29 @@
-class Solution 
-{
-  public int missingNumber(int[] nums) {
-    // Step 1: Find the maximum value inside the array to determine the range
-    int MaxVal = 0;
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] > MaxVal) {
-            MaxVal = nums[i];
-        }
-    }
+import java.util.HashSet;
 
-    // Step 2: Create a new array with MaxVal as the largest value, with descending order from MaxVal to 0
-    int[] descendingArray = new int[MaxVal + 1]; // Size is MaxVal + 1 to include 0
-    for (int i = 0; i <= MaxVal; i++) {
-        descendingArray[i] = MaxVal - i; // Fill from MaxVal down to 0
-    }
-
-    // Step 3: Compare both arrays and find the missing value
-    for (int i = 0; i < nums.length; i++) {
-        for (int j = 0; j < descendingArray.length; j++) {
-            if (nums[i] == descendingArray[j]) {
-                // Once a match is found, mark that number as "found" in descendingArray by setting it to -1
-                descendingArray[j] = -1;
-                break; // Move to the next number in nums
+public class Solution {
+    public int missingNumber(int[] nums) {
+        // Step 1: Find the maximum value in nums
+        int MaxVal = 0;
+        for (int num : nums) {
+            if (num > MaxVal) {
+                MaxVal = num;
             }
         }
-    }
 
-    // Step 4: The missing number is the first -1 we encounter in descendingArray
-    for (int i = 0; i < descendingArray.length; i++) {
-        if (descendingArray[i] != -1) {
-            return descendingArray[i]; // Return the missing number
+        // Step 2: Store nums in a HashSet for quick lookup
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
         }
-    }
 
-    // If no number is missing (i.e., array is complete), return MaxVal + 1
-    return MaxVal + 1;
-}
-      
-    
+        // Step 3: Find the missing number
+        for (int i = 0; i <= MaxVal; i++) {
+            if (!numSet.contains(i)) { // If a number is missing, return it
+                return i;
+            }
+        }
+
+        // Step 4: If no number is missing, return the next possible number
+        return MaxVal + 1;
+    }
 }
