6a3039a27d6d3dca9a95f31a54e033dcdbfb7f6c
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-06-01 09:28:15 -0700

    Time: 49 ms (12.00%) | Memory: 45.1 MB (31.11%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,33 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-     for (int i = 0; i < nums.length - 1; i++)
-     {
-        for (int j = i + 1; j < nums.length; j++)
+class Solution 
+{
+    public int[] twoSum(int[] nums, int target) 
+    {
+        // Declarations
+        int [] indexArray = new int [2];
+
+        int num1 = 0;
+
+        int num2 = 0;
+
+        // Use two pointers to traverse the array
+        for (int i = 0; i < nums.length; i++)
         {
-            if (nums[i] + nums[j] == target)
+            for (int j = i + 1; j < nums.length; j++)
             {
-               return new int[] {i,j};  
+                num1 = nums[i];
+                num2 = nums[j];
+
+                // Check to see if the 2 numbers add up to target and if they are smaller than target individually
+                if (num1 + num2 == target)
+                {
+                    // Add the indexes into the new array and return it
+                    indexArray [0] = i;
+                    indexArray [1] = j;
+                }
             }
         }
-     }
-     return new int[] {};  
+
+        return indexArray;
+        
     }
 }
