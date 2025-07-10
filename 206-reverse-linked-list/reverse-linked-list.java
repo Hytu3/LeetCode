87290099343121f09ddc910ed60ee248f6d224b4
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-07-09 18:22:45 -0700

    Time: 0 ms (100.00%) | Memory: 42.8 MB (19.30%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -8,47 +8,23 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution 
 {
     public ListNode reverseList(ListNode head) 
     {
-        // Variable to keep track of the node
+        ListNode prev = null;
         ListNode current = head;
+        ListNode nextTemp = null;
 
-        // Creating a stack that will hold elements of the original linked list
-        Stack<Integer> stack = new Stack<>();
-
-        // Traverse the list and push values onto the stack
         while (current != null)
         {
-            stack.push(current.val);
-            current = current.next;
+            nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
         }
 
-        // Declare ReverseList as the head of the reversed list
-        ListNode ReverseList = null;
-        ListNode temp = null;
-
-        // Pop elements and build the reversed list
-        while (!stack.isEmpty()) 
-        {
-            int poppedElement = stack.pop();
-            ListNode newNode = new ListNode(poppedElement);
-
-            if (ReverseList == null) 
-            {
-                ReverseList = newNode; // Initialize the reversed list head
-                temp = ReverseList;   // Temp points to the last node added
-            } 
-            else 
-            {
-                temp.next = newNode;  // Link the new node to the reversed list
-                temp = temp.next;     // Move temp to the new node
-            }
-        }
+        return prev;
         
-        return ReverseList;
     }
 }
-
