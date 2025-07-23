956f965e2eb28aa3e29f921921ef634ffe247da2
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-07-22 17:45:12 -0700

    Time: 0 ms (100.00%) | Memory: 40.5 MB (48.97%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public int tribonacci(int n) 
+    {
+      int[] array = new int[n+1];
+
+      // Initialize array based on base cases
+      if (n == 0)
+      {
+        array[0] = 0;
+      }
+      if (n == 1)
+      {
+        array[0] = 0;
+        array[1] = 1;
+      }
+      if (n >= 2)
+      {
+        array[0] = 0;
+        array[1] = 1;
+        array[2] = 1;
+      }
+      
+
+      for (int i = 3; i < array.length; i++)
+      {
+        array[i] = array[i - 3] + array[i - 2] + array [i - 1];
+      } 
+    
+    return array[n];
+        
+    }
+}
