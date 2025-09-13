db79cfdaa69c22551571f830d1a09d774cf547ac
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-09-12 20:16:21 -0700

    Time: 2 ms (95.24%) | Memory: 49.9 MB (96.23%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -2,47 +2,43 @@ class Solution
 {
     public int findMaxConsecutiveOnes(int[] nums) 
     {
-        int maxSize = 0;
 
-        List<int[]> list = new ArrayList<>(); 
-
-        // Break down into subarrys using 0 as the split point
-
-        int start = 0;
+        int count = 0;
         
-        for (int i = 0; i < nums.length; i++) 
+        List<Integer> list = new ArrayList<>();
+
+        int maxVal = 0;
+
+        for (int i = 0; i < nums.length; i++)
         {
-            if (nums[i] == 0) 
+          // Keep count of consecutive ones and place into list and reset count after 0 encountered
+          
+          if (nums[i] != 0)
+          {
+            count++;
+          }
+          else
+          {
+            list.add(count);
+            count = 0;
+          }
+
+        }
+
+        // Returns the last count of consecutive ones in an array
+        list.add(count);
+
+        // Return the max consecutive count
+        
+        for (int j = 0; j < list.size(); j++)
+        {
+            if (list.get(j) > maxVal) 
             {
-                // Add chunk only if it's non-empty
-                if (start < i) 
-                {
-                    list.add(Arrays.copyOfRange(nums, start, i));
-                }
-                start = i + 1; // move start AFTER the zero
+                maxVal = list.get(j);
             }
         }
 
-        // Add the last chunk if it exists
-        if (start < nums.length) 
-        {
-            list.add(Arrays.copyOfRange(nums, start, nums.length));
-        }
-
-        // Track the largest subarray and return its size. This is max consecutive ones.
-        
-        for (int i = 0; i < list.size(); i++) 
-        {
-            int[] subarray = list.get(i);
-            
-            if (subarray.length > maxSize) 
-            {
-                maxSize = subarray.length;
-            }
-
-        }
-
-        return maxSize;
+        return maxVal;
         
     }
 }
