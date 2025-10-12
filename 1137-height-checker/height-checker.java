d175799b0d7a36c2a141c358281c8141181ba615
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-10-11 18:01:33 -0700

    Time: 2 ms (88.48%) | Memory: 41.8 MB (16.37%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -2,11 +2,15 @@ class Solution
 {
     public int heightChecker(int[] heights) 
     {
+        // Make the expected array
+        
         int[] expected = Arrays.copyOf(heights, heights.length);
         Arrays.sort(expected);
 
         int count = 0;
 
+        // Compare them 1 by 1
+        
         for (int i = 0; i < heights.length; i++)
         {
             if (heights[i] != expected[i])
