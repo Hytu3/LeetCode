d60d589a80ee8a4e3ba3b2d11a59b3b1d42b283b
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-10-17 21:21:13 -0700

    Time: 6 ms (4.90%) | Memory: 57.2 MB (87.13%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -6,21 +6,13 @@ class Solution
 
         int carryLimit = nums.length;
         
+        
         for (int i = 0; i < nums.length; i++)
         {
-            int newIndex = (i + k) % nums.length;
+            int newIndex = (i + k) % carryLimit;
             
-            if (newIndex >= carryLimit)
-            {
-                newIndex = newIndex - carryLimit;
-                list.set(newIndex, nums[i]);
-            }
-            
-            if (newIndex < carryLimit)
-            {
-               list.set(newIndex, nums[i]); 
-            }
-            
+            list.set(newIndex, nums[i]); 
+
         }
 
         for (int j = 0; j < nums.length; j++) 
