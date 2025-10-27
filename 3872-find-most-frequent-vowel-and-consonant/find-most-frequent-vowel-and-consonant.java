7a4d405b5ad686003aabd3ae2382c337290612d3
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2025-10-26 22:06:33 -0700

    Time: 4 ms (24.28%) | Memory: 42.7 MB (34.37%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -3,6 +3,7 @@ class Solution
     public int maxFreqSum(String s) 
     {
         HashMap <Character, Integer> map1 = new HashMap<>();
+        HashMap <Character, Integer> map2 = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++)
         {
@@ -19,16 +20,7 @@ class Solution
 
                 map1.put(c,count);
             }
-        }
-
-
-        HashMap <Character, Integer> map2 = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++)
-        {
-            char c = s.charAt(i);
-
-            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u')
+            else
             {
                 int count = 1;
                 
