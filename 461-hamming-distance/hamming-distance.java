632c404230e8c34a529f8d845cde28866be09120
Christian Barreto <123414152+Hytu3@users.noreply.github.com>
2026-01-05 12:00:05 -0700

    Time: 1 ms (7.45%) | Memory: 42.1 MB (75.84%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.java
@@ -0,0 +1,54 @@
+class Solution 
+{
+    public int hammingDistance(int x, int y) 
+    {
+        int count = 0;
+
+        // Turn both start and goal to strings of same length and count mismatching characters
+        String s = Integer.toBinaryString(x);
+        String g = Integer.toBinaryString(y);
+
+        // Logic to ensure both are the same length
+        if (s.length() < g.length())
+        {
+            StringBuilder sb = new StringBuilder(s);
+            
+            while (sb.length() < g.length())
+            {
+                sb.insert(0,0);
+            }
+
+            s = sb.toString();
+
+        }
+
+        if (g.length() < s.length())
+        {
+            StringBuilder sb = new StringBuilder(g);
+            
+            while (sb.length() < s.length())
+            {
+                sb.insert(0,0);         
+            }
+
+            g = sb.toString();
+        }
+
+        // Now that strings are equal track all mismatches
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+
+            char d = g.charAt(i);
+
+            if (c != d)
+            {
+                count++;
+            }
+        }
+
+
+        return count;
+        
+    }
+}
